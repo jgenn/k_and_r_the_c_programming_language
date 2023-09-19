1ca96bbef04dba35660bd39fc6e0fcb90fb2dc51
Jonathan Ennis <ennis.jg@gmail.com>
2023-09-19 08:30:17 -0400

    added 'ex1.c - modified version of hello world program, as solution to Exercise 1-1.

---
+++ ex1.c
@@ -0,0 +1,14 @@
+/*
+ * ex1.c 
+ *
+ * From K&R C, Chapter 1, Exercise 1-1, experimenting with the
+ * 'hello, world' program.
+*/
+#include <stdio.h>
+
+int main() 
+{
+    printf("hello, ");
+    printf("world");
+    printf("\n");
+}
